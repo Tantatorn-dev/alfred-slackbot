df8142bfcdd0a3cfb237f7390b3a90c6e2502cda
Tantatorn <timefear007@gmail.com>
2019-06-03 00:31:04 +0700

    move main.go to new destination

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	api := slack.New("h0ZNuYmbt8d8UhOrUEiOBpzx")
+	api := slack.New("xoxp-629997071139-630017779906-652868024880-2f136fcd4a9a644b662a583f62699c96")
 	user, err := api.GetUserByEmail("tantatorn.s@kmi.tl")
 	if err != nil {
 		fmt.Printf("%s\n", err)
