3ed6829115623e960a0ea2de2f0d81b50d57092f
Tantatorn <timefear007@gmail.com>
2019-06-06 06:53:02 +0700

    add SendMessage method

--- api/slackAPI.go
+++ api/slackAPI.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/nlopes/slack"
 )
 
@@ -9,8 +11,18 @@ type SlackClient struct {
 	API *slack.Client
 }
 
-//Init slack api initialisationS
+//Init slack api initialisation
 func (client *SlackClient) Init() {
 	TOKEN := "xoxb-629997071139-642100825234-KOXW3j2IWEFzAe0tBJ7AaLRi"
 	client.API = slack.New(TOKEN)
+}
+
+//SendMessage a method for sending a message
+func (client *SlackClient) SendMessage(channel string, message string) {
+	channelID, timestamp, err := client.API.PostMessage(channel, slack.MsgOptionText(message, false))
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+	fmt.Printf("Message successfully sent to channel %s at %s\n", channelID, timestamp)
 }
--- main.go
+++ main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"alfred-slackbot/api"
-	"fmt"
-
-	"github.com/nlopes/slack"
 )
 
 func main() {
@@ -12,17 +9,6 @@ func main() {
 	var client api.SlackClient
 
 	client.Init()
-
-	attachment := slack.Attachment{
-		Pretext: "Test",
-		Text:    "Test Test Test",
-	}
-
-	channelID, timestamp, err := client.API.PostMessage("random", slack.MsgOptionText("Some text", false), slack.MsgOptionAttachments(attachment))
-	if err != nil {
-		fmt.Printf("%s\n", err)
-		return
-	}
-	fmt.Printf("Message successfully sent to channel %s at %s\n", channelID, timestamp)
+	client.SendMessage("random", "test slack bot")
 
 }
